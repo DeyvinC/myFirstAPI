139b0c0ca19e2ad4d32969bd0f616e353f40ca23
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-01-31 13:11:24 -0500

    first API

---
+++ index.js
@@ -0,0 +1,12 @@
+// First express api
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('My first Express API.... works!');
+});
+
+
+app.listen(3000, () => console.log(' Listening on port 3000...'));
